0d7953f55b754e2afe4865ad8305a1f4013d60c1
eric <eric.y.corsican@gmail.com>
2021-02-17 00:14:51 +0800

    bin: re-organize features

--- misc/build_cross.sh
+++ misc/build_cross.sh
@@ -3,22 +3,7 @@
 target=$1
 
 if [[ "$target" == *"mips"* ]]; then
-  cross build --release --target $target --manifest-path leaf-bin/Cargo.toml --no-default-features --features "leaf/all-configs leaf/all-endpoints leaf/openssl-aead leaf/openssl-tls"
-#  cross build --release --target $target --manifest-path leaf-bin/Cargo.toml --no-default-features --features "\
-#	  leaf/config-json \
-#	  leaf/inbound-socks \
-#	  leaf/inbound-tun \
-#	  leaf/outbound-chain \
-#	  leaf/outbound-failover \
-#	  leaf/outbound-random \
-#	  leaf/outbound-direct \
-#	  leaf/outbound-drop \
-#	  leaf/outbound-tls \
-#	  leaf/outbound-shadowsocks \
-#	  leaf/outbound-ws \
-#	  leaf/outbound-trojan \
-#	  leaf/openssl-aead \
-#	  leaf/openssl-tls"
+  cross build --release --target $target --manifest-path leaf-bin/Cargo.toml --no-default-features --features "default-openssl"
 else
-  cross build --release --target $target --manifest-path leaf-bin/Cargo.toml --no-default-features --features "leaf/all-configs leaf/all-endpoints leaf/ring-aead leaf/rustls-tls"
+  cross build --release --target $target --manifest-path leaf-bin/Cargo.toml --no-default-features --features "default-ring"
 fi
