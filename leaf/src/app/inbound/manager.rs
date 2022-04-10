2214a54b66fb7ec23206712ec01d11f311b12b01
eric <eric.y.corsican@gmail.com>
2022-04-10 17:05:11 +0800

    inbound/chain: listens only for supported protocols

--- leaf/src/app/inbound/manager.rs
+++ leaf/src/app/inbound/manager.rs
@@ -198,15 +198,21 @@ impl InboundManager {
                         if actors.is_empty() {
                             continue;
                         }
-                        let tcp = Arc::new(chain::inbound::TcpHandler {
-                            actors: actors.clone(),
-                        });
-                        let udp = Arc::new(chain::inbound::UdpHandler { actors });
-                        let handler = Arc::new(proxy::inbound::Handler::new(
-                            tag.clone(),
-                            Some(tcp),
-                            Some(udp),
-                        ));
+                        let tcp = if actors[0].has_tcp() {
+                            let h = Arc::new(chain::inbound::TcpHandler {
+                                actors: actors.clone(),
+                            });
+                            Some(h as crate::proxy::AnyTcpInboundHandler)
+                        } else {
+                            None
+                        };
+                        let udp = if actors[0].has_udp() {
+                            let h = Arc::new(chain::inbound::UdpHandler { actors });
+                            Some(h as crate::proxy::AnyUdpInboundHandler)
+                        } else {
+                            None
+                        };
+                        let handler = Arc::new(proxy::inbound::Handler::new(tag.clone(), tcp, udp));
                         handlers.insert(tag.clone(), handler);
                     }
                     _ => (),
