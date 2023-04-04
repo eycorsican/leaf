677dbf7ed0e7fc4299335a6fb2465ab95dd51939
lemos <xfe1235@gmail.com>
2023-04-04 23:24:24 +0800

    fix(rule): find process by both ip & port

--- leaf/src/app/process_finder.rs
+++ leaf/src/app/process_finder.rs
@@ -1,8 +1,8 @@
+use log::debug;
+use netstat2::{get_sockets_info, AddressFamilyFlags, ProtocolFlags, ProtocolSocketInfo, TcpState};
 use std::net;
 use std::net::IpAddr;
-use log::debug;
-use netstat2::{AddressFamilyFlags, get_sockets_info, ProtocolFlags, ProtocolSocketInfo, TcpState};
-use sysinfo::{ProcessExt, System, SystemExt, Pid, PidExt};
+use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
 
 #[derive(Debug)]
 pub struct PortInfo {
@@ -26,15 +26,15 @@ impl From<netstat2::SocketInfo> for PortInfo {
             ProtocolSocketInfo::Udp(_) => "UDP",
         };
         let system = System::new_all();
-        // system.refresh_system();
         let pid = socket_info.associated_pids.first().unwrap();
-        let process_info = system
-            .process(Pid::from(pid.to_owned() as usize))
-            .map(|p| ProcessInfo {
-                name: p.name().to_owned(),
-                pid: p.pid().as_u32(),
-                process_path: p.exe().to_string_lossy().to_string(),
-            });
+        let process_info =
+            system
+                .process(Pid::from(pid.to_owned() as usize))
+                .map(|p| ProcessInfo {
+                    name: p.name().to_owned(),
+                    pid: p.pid().as_u32(),
+                    process_path: p.exe().to_string_lossy().to_string(),
+                });
         Self {
             address: socket_info.local_addr(),
             port: socket_info.local_port(),
@@ -45,6 +45,7 @@ impl From<netstat2::SocketInfo> for PortInfo {
 }
 
 pub fn find_process(protocol: &str, ip: IpAddr, port: u16) -> Option<PortInfo> {
+    debug!("looking up process for {}:{}:{}", protocol, ip, port);
     let mut af_flags: AddressFamilyFlags = AddressFamilyFlags::from_bits(0).unwrap();
     if ip.is_ipv6() {
         af_flags |= AddressFamilyFlags::IPV6;
@@ -52,7 +53,6 @@ pub fn find_process(protocol: &str, ip: IpAddr, port: u16) -> Option<PortInfo> {
     if ip.is_ipv4() {
         af_flags |= AddressFamilyFlags::IPV4;
     }
-
     let mut proto_flags: ProtocolFlags = ProtocolFlags::from_bits(0).unwrap();
     if protocol == "udp" {
         proto_flags |= ProtocolFlags::UDP;
@@ -60,17 +60,19 @@ pub fn find_process(protocol: &str, ip: IpAddr, port: u16) -> Option<PortInfo> {
     if protocol == "tcp" {
         proto_flags |= ProtocolFlags::TCP;
     }
+    let start_time = tokio::time::Instant::now();
     let sockets = get_sockets_info(af_flags, proto_flags).unwrap_or_default();
-    let mut ports = sockets
+    let mut port_infos = sockets
         .into_iter()
         .filter(|socket_info| match &socket_info.protocol_socket_info {
             ProtocolSocketInfo::Tcp(tcp) => tcp.state != TcpState::Closed,
             ProtocolSocketInfo::Udp(_) => true,
         })
         .map(|socket_info| PortInfo::from(socket_info));
-    let port = ports.find(|p| p.port == port);
-    if let Some(ref p) = port {
-        debug!("find process port {:?}", p);
+    let port_info = port_infos.find(|p| p.address == ip && p.port == port);
+    if let Some(ref p) = port_info {
+        let elapsed = tokio::time::Instant::now().duration_since(start_time);
+        debug!("found process [{}ms] {:?}", elapsed.as_millis(), p);
     }
-    return port;
+    return port_info;
 }
