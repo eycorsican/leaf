7e2619dffcfd556c700cff085f939ba6d6435b0a
Iñigo R. <iruizr7@gmail.com>
2023-09-27 08:20:04 +0200

    FEAT: Implemented UDP SOCKS5 forwarding by hostname

--- leaf/src/proxy/socks/outbound/datagram.rs
+++ leaf/src/proxy/socks/outbound/datagram.rs
@@ -104,12 +104,14 @@ where
                     .map_ok(|_| buf.len())
                     .map_err(|x| Error::new(ErrorKind::Other, x))
                     .await
+            },
+            SocksAddr::Domain(domain, port) => {
+                self.0
+                    .send_to(buf, (domain.to_owned(), *port))
+                    .map_ok(|_| buf.len())
+                    .map_err(|x| Error::new(ErrorKind::Other, x))
+                    .await
             }
-            // FIXME for this, we need our own socks5 impl
-            _ => Err(Error::new(
-                ErrorKind::Other,
-                "socks outbound does not support sending UDP packets to domain address",
-            )),
         }
     }
 
