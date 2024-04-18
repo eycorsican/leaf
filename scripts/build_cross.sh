2b3c4822ab61027a12a000427eb5884a316f0655
eric <eric.y.corsican@gmail.com>
2024-04-18 16:47:21 +0800

    chore: fix build script

--- scripts/build_cross.sh
+++ scripts/build_cross.sh
@@ -3,7 +3,7 @@
 target=$1
 
 if [[ "$target" == *"mips"* ]]; then
-  cross build --release --target $target --manifest-path leaf-bin/Cargo.toml --no-default-features --features "default-openssl"
+  cross build --release --target $target --manifest-path leaf-cli/Cargo.toml --no-default-features --features "default-openssl"
 else
-  cross build --release --target $target --manifest-path leaf-bin/Cargo.toml --no-default-features --features "default-ring"
+  cross build --release --target $target --manifest-path leaf-cli/Cargo.toml --no-default-features --features "default-ring"
 fi
